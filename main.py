f9e9caffbfd103ec2575df12058afbd766353f27
JAKNARONG SONGPATKAEW <C026730@cht-dm.cht.canon.co.th>
2024-06-20 14:57:00 +0700

    update

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import json
 import os.path
+from datetime import datetime
 
 import cv2
 from os import makedirs
 from os.path import join
 
-filename = 'img (1).jpg'
-img = cv2.imread(join(r'C:\PythonProjects\DrawApp\image', filename))
+filename = '001.jpg'
+cap = cv2.VideoCapture('http://192.168.225.137:2000/video')
+_, img = cap.read()
 H, W, _ = img.shape
 
 f = open("frame_dict.json")
@@ -30,6 +32,9 @@ frame = json.loads(string)
 #     cv2.imwrite(fr"C:\PythonProjects\Test  Folder\{name}.png", imgcrop)
 
 def crop_img(img, frame):
+    output_path = join("output")
+    makedirs(output_path, exist_ok=True)
+
     for name, v in frame.items():
         x, y, w, h = v["xywh"]
 
@@ -40,26 +45,10 @@ def crop_img(img, frame):
 
         imgcrop = img[y1:y2, x1:x2]
 
-        path = join("output")
-        makedirs(path, exist_ok=True)
-        image_path = join(path, "images")
+        image_path = join(output_path, name)
         makedirs(image_path, exist_ok=True)
-        label_path = join(path, "labels")
-        makedirs(label_path, exist_ok=True)
+        cv2.imwrite(os.path.join(image_path, datetime.now().strftime('%H%M%S.jpg')), imgcrop)
 
-        cv2.imwrite(os.path.join(image_path, fr"{filename}"), img)
-        f = open(os.path.join(label_path, fr"{filename}".replace(".jpg", ".txt")), "w")
-        f.write(f'0 {x} {y} {w} {h}\n')
 
-crop_img(img, frame)
-
-    # path = (os.path.join("C:\\", "PythonProjects", "Test folder1"))
-    # # print(path)
-    # os.makedirs(path, exist_ok=True)
-    # cv2.imwrite(os.path.join(path ,fr"{name}.png"), imgcrop)
-    #
-    # path = (os.path.join("C:\\", "PythonProjects", "Test folder2"))
-    # os.makedirs(path, exist_ok=True)
-    #
-    # path = (os.path.join("C:\\", "PythonProjects", "Test folder3"))
-    # os.makedirs(path, exist_ok=True)
+if __name__ == '__main__':
+    crop_img(img, frame)
